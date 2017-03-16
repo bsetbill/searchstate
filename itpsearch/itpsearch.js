11ff401cd67ca9ad45b01ae7bf3ad02f55c06542
cutebill <bestbill.act@gmail.com>
2017-03-16 14:27:56 +0800

    change itpsearch to a function

--- itpsearch/itpsearch.js
+++ itpsearch/itpsearch.js
@@ -1,36 +1,20 @@
-function main () {
-  const ARRSIZE = 100
-  const MAX = 100
-  const MIN = 0
-  const SRHNUM = 90
-  let data = []
-  let result = 0
-  data = getdata({arrSize: ARRSIZE})
-  result = search({data: data, max: MAX, min: MIN, srhNum: SRHNUM})
-  console.log(result)
-}
-function getdata ({arrSize = 100} = {}) {
-  let data = []
-  for (let i = 0; i < arrSize; i++) {
-    data[i] = i + 1
-  }
-  return data
-}
-function search ({data = [], max = 100, min = 0, srhNum = 50} = {}) {
+function search ({data = [], max = 100, min = 0, searhNum = 50} = {}) {
   let position = -1
   max = max - 1
   while (true) {
-    let cutPoint = parseInt((max - min) * (srhNum - data[min]) / (data[max] - data[min]) + min)
-    if (data[cutPoint] === srhNum) {
+    const cutPoint = parseInt((max - min) * (searhNum - data[min]) / (data[max] - data[min]) + min)
+    if (data[cutPoint] === searhNum) {
       position = cutPoint
       break
-    } else if (data[cutPoint] > srhNum) {
+    } else if (data[cutPoint] > searhNum) {
       max = cutPoint
     } else {
       min = cutPoint
     }
+    if (max === min || max === min + 1 || isNaN(max) || isNaN(min)) {
+      break
+    }
   }
   return position
 }
-
-main()
+exports.search = search
