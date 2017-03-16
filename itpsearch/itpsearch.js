96d36aa562b80860e7f61e535b240b4b0651c533
cutebill <bestbill.act@gmail.com>
2017-03-16 18:13:43 +0800

    max -> intmax

--- itpsearch/itpsearch.js
+++ itpsearch/itpsearch.js
@@ -1,17 +1,17 @@
 function search ({data = [], max = 100, min = 0, searhNum = 50} = {}) {
   let position = -1
-  max = max - 1
+  let intMax = max - 1
   while (true) {
-    const cutPoint = parseInt((max - min) * (searhNum - data[min]) / (data[max] - data[min]) + min)
+    const cutPoint = parseInt((intMax - min) * (searhNum - data[min]) / (data[intMax] - data[min]) + min)
     if (data[cutPoint] === searhNum) {
       position = cutPoint
       break
     } else if (data[cutPoint] > searhNum) {
-      max = cutPoint
+      intMax = cutPoint
     } else {
       min = cutPoint
     }
-    if (max === min || max === min + 1 || isNaN(max) || isNaN(min)) {
+    if (isNaN(intMax) || isNaN(min) || intMax === min || intMax === min + 1) {
       break
     }
   }
