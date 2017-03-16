6d8b89f5d2c9911c03a92a65a706b4ef7712be8e
cutebill <bestbill.act@gmail.com>
2017-03-16 14:27:31 +0800

    change hashsearch to function

--- hashsearch/hashsearch.js
+++ hashsearch/hashsearch.js
@@ -1,32 +1,12 @@
-function main () {
-  const ARRSIZE = 100
-  const MAX = 100
-  const MIN = 0
-  const SRHNUM = 50
-  const DIVISOR = 23
-  let data = []
-  let result = 0
-  data = getdata({min: MIN, max: MAX, arrSize: ARRSIZE})
-  data = hash({data: data, arrSize: ARRSIZE, divisor: DIVISOR})
-  result = search({data: data, max: MAX, srhNum: SRHNUM, divisor: DIVISOR})
-  console.log(result)
-}
-function getdata ({min = 0, max = 100, arrSize = 100} = {}) {
-  let data = []
-  for (let i = 0; i < arrSize; i++) {
-    data[i] = Math.floor(Math.random() * (max - min + 1)) + min
-  }
-  return data
-}
-function hash ({data = [], arrSize = 100, divisor = 23} = {}) {
-  let hashData = []
+function hash ({data = [], divisor = 23} = {}) {
+  const hashData = []
   let remainder = 0
-  for (let i = 0; i < arrSize; i++) {
+  for (let i = 0; i < data.length; i++) {
     remainder = data[i] % divisor
     if (hashData[remainder] === undefined) {
       hashData[remainder] = data[i]
     } else {
-      for (let j = remainder; j < arrSize; j++) {
+      for (let j = remainder; j < data.length; j++) {
         if (hashData[j] === undefined) {
           hashData[j] = data[i]
           break
@@ -36,19 +16,21 @@ function hash ({data = [], arrSize = 100, divisor = 23} = {}) {
   }
   return hashData
 }
-function search ({data = [], max = 100, srhNum = 50, divisor = 23} = {}) {
+function search ({data = [], searhNum = 50, divisor = 23} = {}) {
   let position = -1
-  let remainder = srhNum % divisor
-  console.log(data)
+  let remainder = searhNum % divisor
   while (true) {
-    if (data[remainder] === srhNum) {
+    if (data[remainder] === searhNum) {
       position = remainder
       break
     } else {
       remainder++
     }
+    if (remainder === data.length) {
+      break
+    }
   }
   return position
 }
-
-main()
+exports.search = search
+exports.hash = hash
